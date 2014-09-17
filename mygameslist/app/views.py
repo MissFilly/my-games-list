d983ac0a4883858a4c1f83c315f6be143e3c6278
MissFilly <lady.filly@gmail.com>
2014-09-17 18:54:59 -0300

    Games list by user, with filter by status

--- mygameslist/app/views.py
+++ mygameslist/app/views.py
@@ -1,7 +1,8 @@
 from django.db.models import Q
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.contrib.auth.models import User
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 from .models import Game, GameReview, GameRecommendation, \
     UserProfile, ListEntry
 
@@ -36,4 +37,18 @@ class GameDetailView(DetailView):
                 user=user,
                 game=self.object
             ).exists()
+        return context
+
+
+class GameListByUserView(ListView):
+    model = ListEntry
+    template_name = 'game_list_by_user.html'
+
+    def get_queryset(self):
+        self.user_profile = get_object_or_404(User, username=self.kwargs['slug'])
+        return ListEntry.objects.filter(user=self.user_profile)
+
+    def get_context_data(self, **kwargs):
+        context = super(GameListByUserView, self).get_context_data(**kwargs)
+        context['user_profile'] = self.user_profile
         return context
--- mygameslist/urls.py
+++ mygameslist/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
-from .app.views import UserDetailView, GameDetailView
+from .app.views import UserDetailView, GameDetailView, GameListByUserView
 
 urlpatterns = patterns(
     '',
@@ -10,6 +10,6 @@ urlpatterns = patterns(
     url(r'^admin/', include(admin.site.urls)),
     url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
         {'document_root': settings.MEDIA_ROOT}),
-    url(r'^game/(?P<pk>\d+)/$', GameDetailView.as_view(), name='game-detail'),
-    url(r'^(?P<slug>[-_\w]+)/$', UserDetailView.as_view(), name='user-detail'),
+    url(r'^game/(?P<pk>\d+)/$', GameDetailView.as_view(), name='game_detail'),
+    url(r'^list/(?P<slug>[-_\w]+)/$', GameListByUserView.as_view(), name='game_list_by_user'),
 )
