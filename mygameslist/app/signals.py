8c14a256bff359d59bee78c9e0e334a8cbc0c7d2
MissFilly <lady.filly@gmail.com>
2014-09-21 04:13:12 -0300

    Fixed amount of allowed entries in recommendations

--- mygameslist/app/signals.py
+++ mygameslist/app/signals.py
@@ -4,7 +4,7 @@ from .models import GameRecommendation
 
 
 def regions_changed(sender, **kwargs):
-    if kwargs['instance'].entries.count() > 3:
+    if kwargs['instance'].entries.count() > 2:
         raise ValidationError("You can't assign more than three regions")
 
 
