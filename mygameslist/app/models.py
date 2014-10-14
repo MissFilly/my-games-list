117063ad3159f70a3e354f1d2bc218e3d1b09617
MissFilly <lady.filly@gmail.com>
2014-10-14 20:46:05 -0300

    User avatar resizing

--- mygameslist/app/models.py
+++ mygameslist/app/models.py
@@ -1,14 +1,16 @@
 import os
 import uuid
 
-from django.db import models
-from django.utils.translation import ugettext as _
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
 from django_countries.fields import CountryField
+from django.db import models
+from django.dispatch import receiver
+from django.utils.translation import ugettext as _
 
 from allauth.account import signals
-from django.dispatch import receiver
+from imagekit.models import ProcessedImageField, ImageSpecField
+from imagekit.processors import ResizeToFit
 
 
 def get_file_path(instance, filename):
@@ -19,14 +21,27 @@ def get_file_path(instance, filename):
 
 class UserProfile(models.Model):
     GENDER_CHOICES = (('F', _('Female')), ('M', _('Male')))
-    avatar = models.ImageField(upload_to=get_file_path,
-                               blank=True, null=True,
-                               verbose_name=_('Avatar'))
+    avatar = ProcessedImageField(upload_to=get_file_path,
+                                 blank=True, null=True,
+                                 processors=[ResizeToFit(265, 320)],
+                                 format='JPEG',
+                                 options={'quality': 90},
+                                 verbose_name=_('Avatar'))
     user = models.OneToOneField(User)
     gender = models.CharField(max_length=1, choices=GENDER_CHOICES,
                               verbose_name=_('Gender'))
     country = CountryField()
     about = models.TextField(verbose_name=_('About'), blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        # Delete old avatar if new avatar was saved
+        try:
+            this = UserProfile.objects.get(id=self.id)
+            if this.avatar != self.avatar:
+                this.avatar.delete(save=False)
+        except:
+            pass
+        super(UserProfile, self).save(*args, **kwargs)
 
     def get_absolute_url(self):
         return reverse('user_profile', kwargs={'slug': self.user.username})
--- mygameslist/settings.py
+++ mygameslist/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = (
     'django_summernote',
     'friendship',
     'storages',
+    'imagekit',
     # My Games list
     'mygameslist.app',
 )
