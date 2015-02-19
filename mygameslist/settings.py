21e2f30746bc62d74aa5899320e0dd9873316f32
MissFilly <lady.filly@gmail.com>
2015-02-18 21:23:37 -0300

    Removing memcache from settings

--- mygameslist/settings.py
+++ mygameslist/settings.py
@@ -186,25 +186,3 @@ SOCIAL_AUTH_STEAM_EXTRA_DATA = ['player']
 
 SOCIAL_AUTH_LOGIN_REDIRECT_URL = '/'
 
-# def get_cache():
-#   import os
-#   try:
-#     os.environ['MEMCACHE_SERVERS'] = os.environ['MEMCACHIER_SERVERS'].replace(',', ';')
-#     os.environ['MEMCACHE_USERNAME'] = os.environ['MEMCACHIER_USERNAME']
-#     os.environ['MEMCACHE_PASSWORD'] = os.environ['MEMCACHIER_PASSWORD']
-#     return {
-#       'default': {
-#         'BACKEND': 'django_pylibmc.memcached.PyLibMCCache',
-#         'TIMEOUT': 500,
-#         'BINARY': True,
-#         'OPTIONS': { 'tcp_nodelay': True }
-#       }
-#     }
-#   except:
-#     return {
-#       'default': {
-#         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache'
-#       }
-#     }
-#
-# CACHES = get_cache()
